1fa048685f36e0b6fa08be06f72e57358476bb88
JoelWukusick <jwukusick@gmail.com>
2020-02-04 22:47:24 -0500

    Setup basic proxy structure

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+let app = express();
+const port = 4000;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.listen(port, () => {
+  console.log(`proxy server listening on port ${port}`);
+})
